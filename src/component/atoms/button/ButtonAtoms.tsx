17dae2f442f7b43991e3507f47521a8d946b5319
細見元気 <hosomi2001@gmail.com>
2024-12-12 23:30:23 +0900

    atoms層に汎用的ボタンを追加。基礎的なテストも記載&突破済み。

---
+++ src/component/atoms/button/ButtonAtoms.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+interface ImgProps {
+  text: string; // 画像のソースを指定するプロパティ
+  onClick: () => void; // クリックイベントを受け取るプロパティ
+  style?: React.CSSProperties; // インラインスタイルを受け取るプロパティ
+  className?: string;
+  disabled?: boolean;
+}
+
+const ButtonAtoms: React.FC<ImgProps> = ({
+  onClick,
+  style,
+  className,
+  disabled,
+}) => {
+  return (
+    <button
+      onClick={disabled ? undefined : onClick}
+      disabled={disabled} // 明示的にdisabledを適用
+      className={className}
+      style={style}
+    />
+  );
+};
+
+export default ButtonAtoms;
