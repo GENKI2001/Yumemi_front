8c708181259652b357fdbd92744ed2bc761ce1c8
細見元気 <hosomi2001@gmail.com>
2024-12-12 10:46:23 +0900

    Headerを定義する。結合テストは未実装。

--- src/component/templates/HomeTemplate.tsx
+++ src/component/templates/HomeTemplate.tsx
@@ -1,9 +1,11 @@
 // ホームのpagesを作成
 import React from 'react';
+import AppHeader from '../organisms/header/AppHeader';
 
 const HomeTemplate: React.FC = () => {
   return (
     <div>
+      <AppHeader img_src="yumemi.png" />
     </div>
   );
 };
