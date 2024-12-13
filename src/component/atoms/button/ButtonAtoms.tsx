f1f1ba92c12dfc929ee76349431eb80c20141a20
細見元気 <hosomi2001@gmail.com>
2024-12-13 18:24:12 +0900

    デザイナーとの連携の取りやすさを考えて、インラインcss -> cssファイルへの変更。

--- src/component/atoms/button/ButtonAtoms.tsx
+++ src/component/atoms/button/ButtonAtoms.tsx
@@ -1,28 +1,25 @@
 import React from 'react';
+import './Button.Atoms.css';
 
 interface ImgProps {
   text: string; // 画像のソースを指定するプロパティ
   onClick: () => void; // クリックイベントを受け取るプロパティ
-  style?: React.CSSProperties; // インラインスタイルを受け取るプロパティ
-  className?: string;
-  disabled?: boolean;
+  className?: string; // クラス名を指定するプロパティ
+  disabled?: boolean; // ボタンの無効化状態
 }
 
 const ButtonAtoms: React.FC<ImgProps> = ({
   text,
   onClick,
-  style,
   className,
   disabled,
 }) => {
   return (
     <button
-      onClick={disabled ? undefined : onClick}
+      onClick={disabled ? undefined : onClick} // disabledの場合はonClickを確実に発火させない
       disabled={disabled} // 明示的にdisabledを適用
-      className={className}
-      style={style}
+      className={`button ${className}`} // 親コンポーネントからクラスを追加可能に
     >
-      {/* 文字をここへ */}
       {text}
     </button>
   );
