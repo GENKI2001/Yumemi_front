5a86d7f1492bd4c9d1a07f0e8257959afd18a31e
細見元気 <hosomi2001@gmail.com>
2024-12-14 10:32:35 +0900

    molecules層のbuttonについて。デザイナー様との共同開発のしやすさを考えて、インラインcss -> cssファイルへの変更。

--- src/component/molecules/button/SelectedWhiteButton.tsx
+++ src/component/molecules/button/SelectedWhiteButton.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import ButtonAtoms from '../../atoms/button/ButtonAtoms';
+import './Button.Molecules.css';
 
 interface LogoProps {
   text: string; // ボタンのテキスト
   isSelected?: boolean; // 選択状態
   onClick: () => void; // クリックイベントを受け取るプロパティ
-  style?: React.CSSProperties; // インラインスタイルを受け取るプロパティ
   disabled?: boolean; // ボタンの無効化状態
 }
 
 const SelectedWhiteButton: React.FC<LogoProps> = ({
   text,
-  style,
   onClick,
-  isSelected,
+  isSelected = true,
   disabled,
 }) => {
   return (
     <ButtonAtoms
       text={text}
-      className={`bg-white ${isSelected ? 'selected' : ''}`}
+      className={`selected-white-button ${isSelected ? 'selected' : ''}`}
       onClick={onClick}
       disabled={disabled}
     />
