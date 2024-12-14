a469e3be22357380c748dc9274a5fb8e0ef560b0
GENKI2001 <96337151+GENKI2001@users.noreply.github.com>
2024-12-14 13:32:07 +0900

    Revert "テスト定義に間違いがあったため、修正。templatesにコメントアウトを追加。"

--- src/component/molecules/checkboxGrid/__tests__/CheckboxGrid.test.tsx
+++ src/component/molecules/checkboxGrid/__tests__/CheckboxGrid.test.tsx
@@ -2,21 +2,23 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import CheckboxGrid from '../CheckboxGrid';
 
 describe('CheckboxGrid Component', () => {
-  const mockOnChange = jest.fn();
   const mockOptions = [
-    { label: 'Option 1', checked: false, onChange: mockOnChange },
-    { label: 'Option 2', checked: true, onChange: mockOnChange },
-    {
-      label: 'Option 3',
-      checked: false,
-      onChange: mockOnChange,
-      disabled: true,
-    },
+    { label: 'Option 1', checked: false },
+    { label: 'Option 2', checked: true },
+    { label: 'Option 3', checked: false, disabled: true },
   ];
+
+  const mockOnChange = jest.fn();
 
   // チェックボックスの個数が正しいことを確認
   test('renders the correct number of checkboxes', () => {
-    render(<CheckboxGrid options={mockOptions} columns={2} />);
+    render(
+      <CheckboxGrid
+        options={mockOptions}
+        onChange={mockOnChange}
+        columns={2}
+      />,
+    );
 
     const checkboxes = screen.getAllByTestId('checkbox-input');
     expect(checkboxes).toHaveLength(mockOptions.length);
@@ -24,7 +26,13 @@ describe('CheckboxGrid Component', () => {
 
   // チェックボックスの各ラベルが正しく表示されることを確認
   test('renders the labels correctly', () => {
-    render(<CheckboxGrid options={mockOptions} columns={2} />);
+    render(
+      <CheckboxGrid
+        options={mockOptions}
+        onChange={mockOnChange}
+        columns={2}
+      />,
+    );
 
     mockOptions.forEach((option) => {
       expect(screen.getByText(option.label)).toBeInTheDocument();
@@ -33,22 +41,50 @@ describe('CheckboxGrid Component', () => {
 
   // チェックボックスがクリックされたときに、正しくonChangeが呼び出されることを確認
   test('calls onChange with the correct arguments when a checkbox is clicked', () => {
-    render(<CheckboxGrid options={mockOptions} columns={2} />);
+    render(
+      <CheckboxGrid
+        options={mockOptions}
+        onChange={mockOnChange}
+        columns={2}
+      />,
+    );
 
     const checkbox = screen.getAllByTestId('checkbox-input')[0];
     fireEvent.click(checkbox);
 
-    expect(mockOnChange).toHaveBeenCalledWith(true);
+    expect(mockOnChange).toHaveBeenCalledWith(0, true);
   });
 
   // オプションに基づいてチェックボックスを正しく無効にすることを確認
   test('disables checkboxes correctly based on the options', () => {
-    render(<CheckboxGrid options={mockOptions} columns={2} />);
+    render(
+      <CheckboxGrid
+        options={mockOptions}
+        onChange={mockOnChange}
+        columns={2}
+      />,
+    );
 
     const checkboxes = screen.getAllByTestId('checkbox-input');
 
     expect(checkboxes[0]).not.toBeDisabled();
     expect(checkboxes[1]).not.toBeDisabled();
     expect(checkboxes[2]).toBeDisabled();
+  });
+
+  // columnsプロップに基づいて正しいグリッドレイアウトをレンダリングすることを確認
+  test('renders the correct grid layout based on the columns prop', () => {
+    const { container } = render(
+      <CheckboxGrid
+        options={mockOptions}
+        onChange={mockOnChange}
+        columns={3}
+      />,
+    );
+
+    const grid = container.firstChild;
+    expect(grid).toHaveStyle(
+      'grid-template-columns: repeat(3, minmax(0, 1fr))',
+    );
   });
 });
