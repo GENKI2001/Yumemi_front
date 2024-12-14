08f84ae4e87a9c3cc9a68df2db97497f6f026d82
細見元気 <hosomi2001@gmail.com>
2024-12-15 00:09:12 +0900

    人口情報をpagesで取得して、templatesにて表示させる。

--- src/component/pages/HomePage.tsx
+++ src/component/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 // ホームのpagesを作成
 import React from 'react';
+import { useGetPopulation } from '../../services/api/population/useGetPopulation';
 import { useGetPrefectures } from '../../services/api/prefecture/useGetPrefecture';
 import HomeTemplate from '../templates/HomeTemplate';
 
 const HomePage: React.FC = () => {
   const { data: prefectures } = useGetPrefectures();
-  return <HomeTemplate prefectures={prefectures} />;
+  const { data: population } = useGetPopulation([1, 12]);
+  return <HomeTemplate prefectures={prefectures} population={population} />;
 };
 
 export default HomePage;
