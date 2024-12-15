1dbe9e0906a648220066243ef4f6bdba6d44dcb2
GENKI2001 <GENKI2001@users.noreply.github.com>
2024-12-15 03:29:13 +0000

    deploy: b8f67a3ba67d891e531339078f560f700fd7bda9

--- src/component/pages/HomePage.tsx
+++
@@ -1,9 +0,0 @@
-// ホームのpagesを作成
-import React from 'react';
-import HomeTemplate from '../templates/HomeTemplate';
-
-const HomePage: React.FC = () => {
-  return <HomeTemplate />;
-};
-
-export default HomePage;
